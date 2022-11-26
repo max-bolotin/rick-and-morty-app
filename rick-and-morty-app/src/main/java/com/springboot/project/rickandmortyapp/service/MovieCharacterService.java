c1dc34d5d364559de334f97e719230f0661c5e99
max-bolotin <realprof.mx@gmail.com>
2022-11-26 16:28:15 +0200

    model, dto and mapper created

--- rick-and-morty-app/src/main/java/com/springboot/project/rickandmortyapp/service/MovieCharacterService.java
+++ rick-and-morty-app/src/main/java/com/springboot/project/rickandmortyapp/service/MovieCharacterService.java
@@ -2,7 +2,6 @@ package com.springboot.project.rickandmortyapp.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface MovieCharacterService {
     void syncExternalCharacters();
 }
